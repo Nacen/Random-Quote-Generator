32f909c5995cc3ab78f50a380daec352f80498bb
Vincent <overlordnacen@gmail.com>
2018-12-24 02:16:26 +0800

    Updates

--- precache-manifest.322bbe6511f67fbbd51ffa2b67ee1917.js
+++ precache-manifest.322bbe6511f67fbbd51ffa2b67ee1917.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Random-Quote-Machine/static/js/runtime~main.67122e2c.js"
   },
   {
-    "revision": "63120702758316dce308",
-    "url": "/Random-Quote-Machine/static/js/main.63120702.chunk.js"
+    "revision": "e6da7ec263e3d07e8511",
+    "url": "/Random-Quote-Machine/static/js/main.e6da7ec2.chunk.js"
   },
   {
     "revision": "8fd1b1351c2a6d3043d2",
     "url": "/Random-Quote-Machine/static/js/1.8fd1b135.chunk.js"
   },
   {
-    "revision": "63120702758316dce308",
+    "revision": "e6da7ec263e3d07e8511",
     "url": "/Random-Quote-Machine/static/css/main.f0061527.chunk.css"
   },
   {
-    "revision": "b8129b814863913d063cfa51b55bb89f",
+    "revision": "fcd555f12902a855b1816f34ccff7a8a",
     "url": "/Random-Quote-Machine/index.html"
   }
 ];
