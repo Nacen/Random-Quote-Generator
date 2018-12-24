fcc9c2bd3cd901dd81e6cbb76f4e29e73c03d6ce
Vincent <overlordnacen@gmail.com>
2018-12-24 13:11:11 +0800

    Added new quotes

--- src/quotes.js
+++ src/quotes.js
@@ -41,6 +41,66 @@ const quotes = [
   {
     text: "The only true wisdom is in knowing you know nothing.",
     author: "Socrates"
-  }
+  },
+  {
+    text: "Life is 10% what happens to you and 90% how you react to it.",
+    author: "Charles R. Swindoll"
+  },
+  {
+    text: "My life is my message.",
+    author: "Mahatma Gandhi"
+  },
+  {
+    text: "Life is what happens when you’re busy making other plans.",
+    author: "John Lennon"
+  },
+  {
+    text: "Believe that life is worth living and your belief will help create the fact.",
+    author: "William James"
+  },
+  {
+    text: "Do not take life too seriously. You will never get out of it alive.",
+    author: "Elbert Hubbard"
+  },
+  {
+    text: "Get busy living or get busy dying.",
+    author: "Stephen King"
+  },
+  {
+    text: "Don’t gain the world and lose your soul, wisdom is better than silver or gold.",
+    author: "Bob Marley"
+  },
+  {
+    text: "To live is the rarest thing in the world. Most people exist, that is all. ",
+    author: "Oscar Wilde"
+  },
+  {
+    text: "Life can only be understood backwards; but it must be lived forwards. ",
+    author: "Soren Kierkegaard"
+  },
+  {
+    text: "Keep your eyes on the stars and your feet on the ground.",
+    author: "Theodore Roosevelt"
+  },
+  {
+    text: "Change your thoughts and you change your world.",
+    author: "Norman Vincent Peale"
+  },
+  {
+    text: "It always seems impossible until it’s done.",
+    author: "Nelson Mandela"
+  },
+  {
+    text: "You only live once, but if you do it right, once is enough",
+    author: "Mae West"
+  },
+  {
+    text: "Whether you think you can, or you think you can't--you're right.",
+    author: "Henry Ford"
+  },
+  {
+    text: "In three words I can sum up everything I've learned about life: it goes on.",
+    author: "Robert Frost"
+  },
 ];
 export default quotes;
