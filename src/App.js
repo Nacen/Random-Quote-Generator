70c90ee0de92d1b81ccae5565d3b4f46cf42fddf
Vincent <overlordnacen@gmail.com>
2018-12-24 14:09:59 +0800

    Add facebook icon from Fontawesome

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Quote from "./components/Quote/Quote";
 import "./App.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 class App extends Component {
-
   render() {
     library.add(faTwitter);
+    library.add(faFacebook);
     return (
       <div>
         <Quote />
